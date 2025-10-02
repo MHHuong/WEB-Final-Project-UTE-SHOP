110d73eecfc09f1c6835d2356c5270c462ae2a6f
TinMH <tinhongmai1012@gmail.com>
2025-10-02 14:41:33 +0700

    Setup env

---
+++ src/main/java/vn/host/UteShopApplication.java
@@ -0,0 +1,12 @@
+package vn.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UteShopApplication.class, args);
+    }
+}
---
+++ src/test/java/vn/host/ute_shop/UteShopApplicationTests.java
@@ -0,0 +1,12 @@
+package vn.host.ute_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
