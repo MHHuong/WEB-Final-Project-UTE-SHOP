116973cf8f77460c67569b982f4f5452efd4dd84
MunKwaii <Quockhanh6310@gmail.com>
2025-10-22 23:46:32 +0700

    Update ProductService.java
    Delete unnecessary code from ProductService

--- src/main/java/vn/host/service/ProductService.java
+++ src/main/java/vn/host/service/ProductService.java
@@ -5,10 +5,5 @@ import vn.host.entity.Product;
 import java.util.List;
 
 public interface ProductService {
-    void save(Product product);
-    void delete(long id);
-    List<Product> findAll();
-    Product findById(long id);
-    List<Product> findByShopId(long shopId);
-    List<Product> findByCategoryId(long id);
+
 }
