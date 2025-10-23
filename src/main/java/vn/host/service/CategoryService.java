987946b5ef3cedd463ac220181b3d872176e55ba
MunKwaii <Quockhanh6310@gmail.com>
2025-10-23 14:12:07 +0700

    Delete unnecessary code from CategoryService

--- src/main/java/vn/host/service/CategoryService.java
+++ src/main/java/vn/host/service/CategoryService.java
@@ -5,8 +5,5 @@ import vn.host.entity.Category;
 import java.util.List;
 
 public interface CategoryService {
-    void save(Category category);
-    void delete(long id);
-    List<Category> findAll();
-    Category findById(long id);
+   
 }
