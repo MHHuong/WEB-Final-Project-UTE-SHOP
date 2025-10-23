2726a9313316584d950fd6e8b1ed0042985447e0
MunKwaii <Quockhanh6310@gmail.com>
2025-10-23 14:13:03 +0700

    Delete unnecessary code from CategoryRepository

--- src/main/java/vn/host/repository/CategoryRepository.java
+++ src/main/java/vn/host/repository/CategoryRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long>, JpaSpecificationExecutor<Category> {
-    List<Category> findByParent_CategoryId(Long parentId);
-    boolean existsByName(String name);
+
 }
