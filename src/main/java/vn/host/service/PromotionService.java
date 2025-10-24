1ada95e3eaf448fd01f97d95d98f93ec8e43a383
MunKwaii <Quockhanh6310@gmail.com>
2025-10-24 11:02:59 +0700

    Update PromotionService.java

--- src/main/java/vn/host/service/PromotionService.java
+++ src/main/java/vn/host/service/PromotionService.java
@@ -1,12 +1,6 @@
 package vn.host.service;
 
-import vn.host.entity.Promotion;
-
 import java.util.List;
 
 public interface PromotionService {
-    void save(Promotion promotion);
-    void delete(long id);
-    List<Promotion> findAll();
-    Promotion findById(long id);
 }
