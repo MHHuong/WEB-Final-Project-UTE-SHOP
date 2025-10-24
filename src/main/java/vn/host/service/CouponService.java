b3317e32b97891e1a714afa69f642c3ee23cc214
MunKwaii <Quockhanh6310@gmail.com>
2025-10-24 11:02:41 +0700

    Update CouponService.java

--- src/main/java/vn/host/service/CouponService.java
+++ src/main/java/vn/host/service/CouponService.java
@@ -1,12 +1,6 @@
 package vn.host.service;
 
-import vn.host.entity.Coupon;
-
 import java.util.List;
 
 public interface CouponService {
-    List<Coupon> findAll();
-    Coupon findById(long id);
-    void save(Coupon coupon);
-    void delete(long id);
 }
