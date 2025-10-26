7600053eb2a623e3b97a0a74cdcdc74b0bb6641a
MunKwaii <Quockhanh6310@gmail.com>
2025-10-26 11:53:08 +0700

    Update ShippingProviderService.java

--- src/main/java/vn/host/service/ShippingProviderService.java
+++ src/main/java/vn/host/service/ShippingProviderService.java
@@ -5,8 +5,4 @@ import vn.host.entity.ShippingProvider;
 import java.util.List;
 
 public interface ShippingProviderService {
-    void save(ShippingProvider shippingProvider);
-    void delete(long id);
-    List<ShippingProvider> findAll();
-    ShippingProvider findById(long id);
 }
