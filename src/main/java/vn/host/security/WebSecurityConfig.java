36eebc70bc60ef72f42ee94b2ba7063fd3f54f8d
TinMH <tinhongmai1012@gmail.com>
2025-10-26 18:55:32 +0700

    Add forgot password

--- src/main/java/vn/host/security/WebSecurityConfig.java
+++ src/main/java/vn/host/security/WebSecurityConfig.java
@@ -3,13 +3,9 @@ package vn.host.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -37,7 +33,8 @@ public class WebSecurityConfig {
                                 "/pages/**",
                                 "/api/auth/**",
                                 "/login",
-                                "/register"
+                                "/register",
+                                "/forgot-password"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
