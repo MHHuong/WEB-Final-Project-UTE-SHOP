f41f3e559f3aaea259b38898827fdae5cdcbdadf
minhat5 <23110265@student.hcmute.edu.vn>
2025-10-28 21:18:12 +0700

    change pagecontroller into shoppagecontroller

--- src/main/java/vn/host/controller/shop/ShopPageController.java
+++ src/main/java/vn/host/controller/shop/ShopPageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
+public class ShopPageController {
     @GetMapping("/shop")
     public String shopIndex() {
         return "shop/index";
